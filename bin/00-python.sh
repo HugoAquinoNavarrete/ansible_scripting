d18d1064b60b49e3f5412e4a6d61db4a98d18839
HugoAquinoNavarrete <hugo.aquino.navarrete@gmail.com>
2021-05-09 14:57:34 -0500

    Actualizando inventario así como YML y shell

--- bin/00-python.sh
+++ bin/00-python.sh
@@ -1,1 +1,2 @@
 ansible-playbook -i ./ansible_inventario.txt ./playbooks/python.yml --private-key=./key_lab_jenkins -e"host=servers" -v -u ubuntu
+ansible-playbook -i ./ansible_inventario.txt ./playbooks/python.yml --private-key=./key_lab -e"host=servers_otrasubred" -v -u ubuntu
